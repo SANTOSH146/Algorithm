9f5ce64462f679f4b08a8cf8fd8fad5092142199
Santosh Kumar DONDAPATI <santoshk6r@gmail.com>
2026-02-05 18:40:27 +0530

    Selection sorting algo implementation

---
+++ Sorting/selectionsort.java
@@ -0,0 +1,39 @@
+package Sorting;
+import java.util.*;
+
+class selectionSort{
+
+    public static void swapNumbers(int i,int j,int[] arr){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+        }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+        
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            swapNumbers(i, min, arr);
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+}
+
+
+// Time: O(n²)
+//Space: O(1) 
