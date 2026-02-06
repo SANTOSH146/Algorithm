8c72231ddb5f87fa9dc33df38afdad0247c5f7b4
Santosh Kumar DONDAPATI <santoshk6r@gmail.com>
2026-02-06 13:08:43 +0530

    bubble sort algo implememtation

---
+++ Sorting/bubblesort.java
@@ -0,0 +1,36 @@
+package Sorting;
+import java.util.*;
+
+class bubblesort {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+
+        for(int i=n-1;i>=0;i--){
+            int swapcount=0;
+            for(int j=0;j<i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swapcount++;
+                }
+                
+                
+            }
+            if(swapcount==0){
+                    break;
+                }
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        
+    }
+}
